9fc64cccb75f18a0e61d9c6d60119dbd2279f460
Yukti Madariya <yukti.madariya@gmail.com>
2025-08-05 23:39:46 +0530

    Update List.jsx

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,32 +1,69 @@
-import React, { useEffect, useState } from "react";
-import CardGroup from "react-bootstrap/CardGroup";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-import BookCard from "../components/Card";
 import { useFirebase } from "../context/Firebase";
 
-const HomePage = () => {
+const ListingPage = () => {
   const firebase = useFirebase();
 
-  const [books, setBooks] = useState([]);
+  const [name, setName] = useState("");
+  const [isbnNumber, setIsbnNumber] = useState("");
+  const [price, setPrice] = useState("");
+  const [coverPic, setCoverPic] = useState("");
 
-  useEffect(() => {
-    firebase.listAllBooks().then((books) => setBooks(books.docs));
-  }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await firebase.handleCreateNewListing(name, isbnNumber, price, coverPic);
+  };
 
   return (
     <div className="container mt-5">
-      <CardGroup>
-        {books.map((book) => (
-          <BookCard
-            link={`/book/view/${book.id}`}
-            key={book.id}
-            id={book.id}
-            {...book.data()}
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Enter Book Name</Form.Label>
+          <Form.Control
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            type="text"
+            placeholder="Book name"
           />
-        ))}
-      </CardGroup>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>ISBN</Form.Label>
+          <Form.Control
+            onChange={(e) => setIsbnNumber(e.target.value)}
+            value={isbnNumber}
+            type="text"
+            placeholder="ISBN Number"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Price</Form.Label>
+          <Form.Control
+            onChange={(e) => setPrice(e.target.value)}
+            value={price}
+            type="text"
+            placeholder="Enter Price"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Cover Pic</Form.Label>
+          <Form.Control
+            onChange={(e) => setCoverPic(e.target.files[0])}
+            type="file"
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Create
+        </Button>
+      </Form>
     </div>
   );
 };
 
-export default HomePage;
+export default ListingPage;
